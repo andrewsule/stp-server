b0b0cb91f8175fa9a1ef511f3b498ade0a4a6b1b
unknown <andrewsuleyengi50@gmail.com>
2023-07-23 11:33:41 +0530

    changes

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ mongoose.connection.on("error", (err) => {
   console.error("Failed to connect to MongoDB", err);
 });
 
-const hostname = process.env.HOST_NAME;
-const port = process.env.PORT;
+const port = process.env.PORT||5000;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
@@ -30,6 +29,6 @@ app.get("/", (req, res) => {
   res.json();
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Express server is started at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Express server is started at ${port}`);
 });
